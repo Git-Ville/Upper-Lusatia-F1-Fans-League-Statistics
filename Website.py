9b62ab17233fb32678b1cf0bfff2e57419270959
Git-Ville <133105923+Git-Ville@users.noreply.github.com>
2024-09-14 16:59:56 +0200

    Update Website.py

--- Website.py
+++ Website.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 #Erstellung Website
 st.set_page_config(page_title="Statistiken Upper Lusatia F1 Fans", layout="wide")
+
+# Hinzufügen von Inhalt
+st.title("Willkommen auf der Website der F1 Fans in Upper Lusatia!")
+st.write("Hier findest du Statistiken und Informationen zu F1-Fans in Upper Lusatia.")
