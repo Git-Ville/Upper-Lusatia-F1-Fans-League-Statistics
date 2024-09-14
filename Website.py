392b310c69d479d9618884a1640f1621f162bf09
Git-Ville <133105923+Git-Ville@users.noreply.github.com>
2024-09-14 18:13:43 +0200

    Update Website.py

--- Website.py
+++ Website.py
@@ -6,3 +6,6 @@ st.set_page_config(page_title="Statistiken Upper Lusatia F1 Fans", layout="cente
 # Hinzufügen von Inhalt
 st.title("Willkommen auf der Website der F1 Fans in Upper Lusatia!")
 st.write("Hier findest du Statistiken und Informationen zu F1-Fans in Upper Lusatia.")
+st.header("Qualifying", divider=True)
+st.header("Rennen", divider=True)
+st.header("Hall of Fame", divider=True)
