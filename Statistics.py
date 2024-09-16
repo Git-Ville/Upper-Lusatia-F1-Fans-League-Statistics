5754a7c8786e25faf8ca2bc2a2f3aeb68d085a20
Git-Ville <133105923+Git-Ville@users.noreply.github.com>
2024-09-16 18:23:38 +0200

    Aserbaidschan Update

--- Statistics.py
+++ Statistics.py
@@ -1,37 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Statistiken der »UPPER LUSATIA Fantasy F1 FANS« Liga 2024
-
-# In[1]:
-
-
 # Hier importieren wir alle notwendigen Module
 
 import pandas as pd
+import os 
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-# In[15]:
-
-
 # Hier lesen wir die Daten ein
-data_quali = pd.read_csv("Data/Quali.csv", encoding="utf-8", sep=",", index_col=0)
+data_quali = pd.read_csv("Users/juliu/Documents/Hobbys/Upper-Lusatia-F1-Fans-League-Statistics/Data/quali.csv", encoding="utf-8", sep=",", index_col=0)
 
-
-# In[16]:
 
 
 #alle Werte zu int
 data_quali = data_quali.astype(int)
-
-
-# ## Qualifying
-
-# Anzahl der Pole Positions (meiste Punkte im Qualifying):
-
-# In[34]:
 
 
 pole_position_counts = {}
@@ -65,23 +45,11 @@ pole_position_df.index.name = 'Team'
 pole_position_df.sort_values(by="Pole Positions", inplace=True, ascending=False)
 
 
-# ## Rennen
-
-# In[6]:
-
-
 # Hier lesen wir die Daten ein
 data_rennen = pd.read_csv("Data/Rennen.csv", encoding="utf-8", sep=",", index_col=0)
 
-
-# In[7]:
-
-
 #alle Werte zu int
 data_rennen = data_rennen.astype(int)
-
-
-# In[8]:
 
 
 Rennsiege_counts = {}
@@ -114,40 +82,6 @@ Rennsiege_df.index.name = 'Team'
 Rennsiege_df.sort_values(by="Rennsiege", inplace=True, ascending=False)
 
 
-# In[26]:
-
-
-# Diagramm erstellen
-fig, ax = plt.subplots(figsize=(12, 6))
-
-# Balken für Pole-Positionen (nach links)
-ax.barh(pole_position_df.index, pole_position_df['Pole Positions'], color='skyblue', label='Pole-Positionen', align='center')
-
-# Balken für Rennsiege (nach rechts)
-ax.barh(Rennsiege_df.index, -Rennsiege_df['Rennsiege'], color='orange', label='Rennsiege', align='center')
-
-# Achsenbeschriftungen und Titel
-ax.set_xlabel('Anzahl')
-ax.set_ylabel('Team')
-ax.set_title('Anzahl der Pole-Positionen und Rennsiege pro Team')
-
-# Legende
-ax.legend()
-
-# Y-Achse in die Mitte verschieben
-ax.spines['left'].set_position(('axes', 0.45))
-
-ax.xaxis.set_major_formatter(lambda x, pos: f'{abs(x):g}')
-
-
-# Diagramm anzeigen
-plt.tight_layout()
-plt.show()
-
-
-# In[27]:
-
-
 #Hier erstellen wir eine neues Dataframe um die Gesamtpunkte anzuzeigen
 rennen_gesamt = pd.DataFrame()
 
@@ -165,69 +99,17 @@ rennen_gesamt.sort_values(by=rennen_gesamt.columns[-1], inplace=True, ascending=
 
 # Saisonverlauf (Punkte der Rennwochenenden addiert)
 
-# In[28]:
-
-
 # Transponieren des DataFrames
 rennen_gesamt_transponiert = rennen_gesamt.transpose()
-
-# Erstellen des Liniendiagramms
-rennen_gesamt_transponiert.plot(kind='line', figsize=(12, 6))
-
-# Anpassen der Intervalle auf der x-Achse
-plt.xticks(range(1, len(rennen_gesamt_transponiert) + 1))
-
-#Grid
-plt.grid(True)
-
-# Anpassen der Achsenbeschriftungen und des Titels
-plt.xlabel('Rennen')
-plt.ylabel('Punkte')
-plt.title('Gesamtpunkte')
-
-# Anzeigen des Diagramms
-plt.show()
-
-
-# In[29]:
-
 
 rennen_gesamtrang = rennen_gesamt.rank(ascending=False)
 rennen_gesamtrang = rennen_gesamtrang.astype("int")
 rennen_gesamtrang.sort_values(by=rennen_gesamtrang.columns[-1], inplace=True)
 
 
-# In[30]:
-
-
 # Transponieren des DataFrames
 rennen_gesamtrang_transponiert = rennen_gesamtrang.transpose()
 rennen_gesamtrang_transponiert = rennen_gesamtrang_transponiert * -1
-
-# Erstellen des Liniendiagramms
-rennen_gesamtrang_transponiert.plot(kind='line', figsize=(12, 6))
-
-plt.gca().invert_yaxis()
-
-# Anpassen der Achsenbeschriftungen und des Titels
-plt.xlabel('Rennen')
-plt.ylabel('Platz')
-plt.title('Platzierungsverlauf')
-
-# Anzeigen des Diagramms
-plt.grid(True)
-
-# Legende am rechten Rand platzieren
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-
-# Anpassen der Intervalle auf der x-Achse
-plt.xticks(range(1, len(rennen_gesamtrang_transponiert) + 1))
-        
-# Anzeigen des Diagramms
-plt.show()
-
-
-# In[31]:
 
 
 #Hier erstellen wir eine neue Zeile, welche den Durchschnitt der Liga abbildet
@@ -236,12 +118,6 @@ average = rennen_gesamt.mean()
 
 data_rennen_average.loc[11] = average
 data_rennen_average = data_rennen_average.rename(index={11: 'Average'})
-
-
-# In[32]:
-
-
-import pandas as pd
 
 # Hier erstellen wir ein neues DataFrame, welches den Abstand zum Durchschnitt abbildet
 Abstände = pd.DataFrame()
@@ -262,33 +138,5 @@ Abstände.columns = range(1, len(Abstände.columns) + 1)
 
 Abstände.sort_values(Abstände.columns[-1], inplace=True, ascending=False)
 
-
-# In[33]:
-
-
 # Transponieren des DataFrames
 Abstände_transponiert = Abstände.transpose()
-
-# Erstellen des Liniendiagramms
-Abstände_transponiert.plot(kind='line', figsize=(12, 6))
-
-#plt.gca().invert_yaxis()
-
-# Anpassen der Achsenbeschriftungen und des Titels
-plt.xlabel('Rennen')
-plt.ylabel('Punkte')
-plt.title('Abstände')
-
-# Anzeigen des Diagramms
-plt.grid(True)
-
-# Legende am rechten Rand platzieren
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-
-# Anpassen der Intervalle auf der x-Achse
-plt.xticks(range(1, len(Abstände_transponiert) + 1))
-
-        
-# Anzeigen des Diagramms
-plt.show()
-
--- Website.py
+++ Website.py
@@ -1,24 +1,26 @@
 import streamlit as st
 import pandas as pd
 
-#Erstellung Website
+# set Website
 st.set_page_config(page_title="Statistiken Upper Lusatia F1 Fans", layout="centered")
 
 
 
-# Hinzufügen von Inhalt
+# add title and explanation
 st.title("Willkommen auf der Website der F1 Fans in Upper Lusatia!")
 st.write("Hier findest du Statistiken und Informationen zu F1-Fans in Upper Lusatia.")
 
-tab1, tab2, tab3 = st.tabs(["Qualifying", "Race", "Hall of Fame"])
+# add tabs
+tab1, tab2, tab3 = st.tabs(["🔴 Qualifying", "🟢 Race", "🟡 Hall of Fame"])
 
+#tab1
 with tab1:
-    st.header("Qualifying", divider=True)
+    st.header("Qualifying", divider="red")
     from Statistics import pole_position_df
     st.bar_chart(pole_position_df)
 
 with tab2:
-    st.header("Race", divider=True)
+    st.header("Race", divider="green")
     from Statistics import Rennsiege_df
     st.bar_chart(Rennsiege_df)
     from Statistics import rennen_gesamt_transponiert
@@ -29,4 +31,4 @@ with tab2:
     st.line_chart(Abstände_transponiert)
 
 with tab3:
-    st.header("Hall of Fame", divider=True)
+    st.header("Hall of Fame", divider="yellow")
